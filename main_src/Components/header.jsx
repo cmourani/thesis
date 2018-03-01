cdbfb36158730e402fad50202927867ff2b36fa5
CaioAR <cmourani12@yahoo.com>
2018-02-28 22:07:49 -0800

    changed header link

--- main_src/Components/header.jsx
+++ main_src/Components/header.jsx
@@ -13,7 +13,7 @@ const tabStyle = {
 
 class Header extends Component {
   titleClick() {
-    window.location.href = ('http://ec2-18-219-85-110.us-east-2.compute.amazonaws.com/dashboard')
+    window.location.href = ('http://host-ly.herokuapp.com//dashboard')
   }
   render() {
     return (
